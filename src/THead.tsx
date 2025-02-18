fa42b9337d6bb1c54790c63fa08e662f05b5ccef
Chee3se <emilspete999@gmail.com>
2025-02-18 12:33:12 +0200

    Gherkin fix 6

--- src/THead.tsx
+++ src/THead.tsx
@@ -1,10 +1,10 @@
 export default function THead () {
     return (
-        <thead className="bg-gradient-to-r from-green-400 to-green-600 text-white">
+        <thead className="bg-gradient-to-r from-green-700 to-green-800 text-white">
         <tr>
-            <th className="px-6 py-4 border-b border-green-700 font-semibold text-left">Nr</th>
-            <th className="px-6 py-4 border-b border-green-700 font-semibold text-left">English</th>
-            <th className="px-6 py-4 border-b border-green-700 font-semibold text-left">Latvian</th>
+            <th className="px-6 py-4 border-b border-green-800 font-semibold text-left">Nr</th>
+            <th className="px-6 py-4 border-b border-green-800 font-semibold text-left">English</th>
+            <th className="px-6 py-4 border-b border-green-800 font-semibold text-left">Latvian</th>
         </tr>
         </thead>
     )
